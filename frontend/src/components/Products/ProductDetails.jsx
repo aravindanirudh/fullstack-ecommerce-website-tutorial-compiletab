02930c103d3878833f0b330f136ca198910c0d24
aravindanirudh1@gmail.com <aravindanirudh1@gmail.com>
2025-12-10 22:06:52 +0530

    2:56:40 of tutorial completed

--- frontend/src/components/Products/ProductDetails.jsx
+++ frontend/src/components/Products/ProductDetails.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { toast } from 'sonner';
+import ProductGrid from './ProductGrid';
 
 const selectedProduct = {
     name: "Stylish Jacket",
@@ -22,7 +23,34 @@ const selectedProduct = {
     ],
 };
 
-export const ProductDetails = () => {
+const similarProducts = [
+    {
+        _id: 1,
+        name: "Product 1",
+        price: 100,
+        images: [{ url: "https://picsum.photos/300/400?random=3" }],
+    },
+    {
+        _id: 2,
+        name: "Product 2",
+        price: 100,
+        images: [{ url: "https://picsum.photos/300/400?random=4" }],
+    },
+    {
+        _id: 3,
+        name: "Product 3",
+        price: 100,
+        images: [{ url: "https://picsum.photos/300/400?random=5" }],
+    },
+    {
+        _id: 4,
+        name: "Product 4",
+        price: 100,
+        images: [{ url: "https://picsum.photos/300/400?random=6" }],
+    },
+];
+
+const ProductDetails = () => {
     const [mainImage, setMainImage] = React.useState("");
     const [selectedSize, setSelectedSize] = React.useState("");
     const [selectedColor, setSelectedColor] = React.useState("");
@@ -144,7 +172,15 @@ export const ProductDetails = () => {
                     </div>
                 </div>
             </div>
+            <div className="mt-20">
+                <h2 className='text-2xl text-center font-medium mb-4'>
+                    You May Also Like
+                </h2>
+                <ProductGrid products={similarProducts}/>
+            </div>
         </div>
     </div>
   )
 }
+
+export default ProductDetails
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Hero from '../components/Layout/Hero'
 import GenderCollectionSection from '../components/Products/GenderCollectionSection'
 import NewArrivals from '../components/Products/NewArrivals'
-import { ProductDetails } from '../components/Products/ProductDetails'
+import ProductDetails from '../components/Products/ProductDetails'
 
 const Home = () => {
   return (
