71c93f309ea1b1be39cd2b2ab86b7d81d7e728c1
aravindanirudh1@gmail.com <aravindanirudh1@gmail.com>
2026-01-02 19:57:14 +0530

    03:24:19 of tutorial completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import UserLayout from "./components/Layout/UserLayout";
 import Home from "./pages/Home";
 import { Toaster } from "sonner";
+import Login from "./pages/Login";
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
       {/* Path refers to the URL path and the element refers to the component to be rendered */}
       <Route path="/" element={<UserLayout />}>
         <Route index element={<Home />} />
+        <Route path="login" element={<Login />} />
       </Route>
       <Route>{/* Admin Layout */}</Route>
      </Routes>
