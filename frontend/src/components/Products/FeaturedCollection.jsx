a0fb61a6cb7893beb507943c32cb38761f1ebea3
aravindanirudh1@gmail.com <aravindanirudh1@gmail.com>
2026-01-02 15:50:17 +0530

    03:13:17 of tutorial completed - Homepage is ready

--- frontend/src/components/Products/FeaturedCollection.jsx
+++ frontend/src/components/Products/FeaturedCollection.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import featured from "../../assets/featured.webp"
 
 const FeaturedCollection = () => {
   return (
@@ -11,6 +12,11 @@ const FeaturedCollection = () => {
                 <h2 className="text-4xl lg:text-5xl font-bold mb-6">Apparel made for your everyday life</h2>
                 <p className="text-lg text-gray-600 mb-6">Discover high-quality, comfortable clothing that effortlessly blends fashion and function. Designed to make you look and feel great every day.</p>
                 <Link to="/collections/all" className="bg-black text-white px-6 py-3 rounded-lg text-lg hover:bg-gray-800">Shop Now</Link>
+            </div>
+
+            {/* Right content */}
+            <div className="lg:w-1/2">
+                <img src={featured} alt="Featured Collection" className='w-full h-full object-cover lg:round-tr-3xl lg:rounded-br-3xl'/>
             </div>
         </div>
     </section>
