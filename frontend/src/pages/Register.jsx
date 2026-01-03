2a638cc3d86a473c0d49cd22aa8efb9d59179f1f
aravindanirudh1@gmail.com <aravindanirudh1@gmail.com>
2026-01-03 13:24:58 +0530

    03:29:05 of tutorial completed

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
-import login from '../assets/login.webp';
+import register from '../assets/register.webp';
 
-const Login = () => {
+const Register = () => {
     const [email, setEmail] = React.useState("");
     const [password, setPassword] = React.useState("");
+    const [name, setName] = React.useState("");
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log("User login: ", { email, password });
+        console.log("User registered: ", { name, email, password });
     }
 
   return (
@@ -21,6 +22,10 @@ const Login = () => {
                 <h2 className='text-2xl font-bold text-center mb-6'>Hey there! 👋🏻</h2>
                 <p className="text-center mb-6">Enter your username and password to login</p>
                 <div className="mb-4">
+                    <label className='block text-sm font-semibold mb-2'>Name</label>
+                    <input type="text" value={name} onChange={(e) => setName(e.target.value)} className='w-full p-2 border border-gray-200 rounded-md' placeholder='Enter your name'/>
+                </div>
+                <div className="mb-4">
                     <label className='block text-sm font-semibold mb-2'>Email</label>
                     <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} className='w-full p-2 border border-gray-200 rounded-md' placeholder='Enter your email address'/>
                 </div>
@@ -28,21 +33,21 @@ const Login = () => {
                     <label className='block text-sm font-semibold mb-2'>Password</label>
                     <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className='w-full p-2 border border-gray-200 rounded-md' placeholder='Enter your password'/>
                 </div>
-                <button type='submit' className='w-full bg-black text-white p-2 rounded-lg font-semibold hover:bg-gray-800 transition'>Sign In</button>
+                <button type='submit' className='w-full bg-black text-white p-2 rounded-lg font-semibold hover:bg-gray-800 transition'>Sign Up</button>
                 <p className="mt-6 text-center text-sm">
                     Don't have an account?{" "}
-                    <Link to="/register" className="text-blue-500">Register</Link>
+                    <Link to="/login" className="text-blue-500">Login</Link>
                 </p>
             </form>
         </div>
 
         <div className='hidden md:block w-1/2 bg-gray-800'>
             <div className="h-full flex flex-col justify-center items-center">
-                <img src={login} alt="Login to account" className='h-[750px] w-full object-cover'/>
+                <img src={register} alt="Login to account" className='h-[750px] w-full object-cover'/>
             </div>
         </div>
     </div>
   )
 }
 
-export default Login
+export default Register
