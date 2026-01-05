46a766fbf7b8b02e9e9d56b2caea2b33bc5318e4
aravindanirudh1@gmail.com <aravindanirudh1@gmail.com>
2026-01-05 18:22:05 +0530

    04:56:00 of tutorial completed

--- frontend/src/components/Layout/CartDrawer.jsx
+++ frontend/src/components/Layout/CartDrawer.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 import { IoMdClose } from 'react-icons/io'
 import CartContents from '../Cart/CartContents'
 
 const CartDrawer = ({ drawerOpen, toggleCartDrawer }) => {
+  const navigate = useNavigate();
+  const handleCheckout = () => {
+    navigate('/checkout');
+  }
+
   return (
     <div
       className={`fixed top-0 right-0 h-full w-3/4 sm:w-1/2 md:w-120 bg-white shadow-lg transform transition-transform duration-300 flex flex-col z-50
@@ -23,7 +29,7 @@ const CartDrawer = ({ drawerOpen, toggleCartDrawer }) => {
 
       {/* Checkout button */}
       <div className="p-4 bg-white sticky bottom-0">
-        <button className="w-full bg-black text-white py-3 rounded-lg font-semibold hover:bg-gray-800 transition">
+        <button className="w-full bg-black text-white py-3 rounded-lg font-semibold hover:bg-gray-800 transition" onClick={handleCheckout}>
           Checkout
         </button>
         <p className="text-sm tracking-tight text-gray-500 mt-2 text-center">
