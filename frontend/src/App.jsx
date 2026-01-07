ab366d79224db4189737824377245115d0b519cc
aravindanirudh <aravindanirudh1@gmail.com>
2026-01-07 23:49:20 +0530

    07:09:35 of tutorial completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,6 +15,7 @@ import AdminLayout from "./components/Admin/AdminLayout";
 import AdminHomePage from "./pages/AdminHomePage";
 import UserManagement from "./components/Admin/UserManagement";
 import ProductManagement from "./components/Admin/ProductManagement";
+import EditProductPage from "./components/Admin/EditProductPage";
 
 function App() {
 
@@ -40,6 +41,7 @@ function App() {
         <Route index element={<AdminHomePage />} />
         <Route path="users" element={<UserManagement />} />
         <Route path="products" element={<ProductManagement />} />
+        <Route path="products/:id/edit" element={<EditProductPage />} />
       </Route>
      </Routes>
     </BrowserRouter>
