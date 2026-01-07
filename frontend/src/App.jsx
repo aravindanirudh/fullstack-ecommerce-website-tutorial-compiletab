24b479b0b43e001551df21e182f0a8985ff16131
aravindanirudh <aravindanirudh1@gmail.com>
2026-01-07 21:12:27 +0530

    06:30:14 of tutorial completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,6 +13,7 @@ import OrderDetailsPage from "./pages/OrderDetailsPage";
 import MyOrdersPage from "./pages/MyOrdersPage";
 import AdminLayout from "./components/Admin/AdminLayout";
 import AdminHomePage from "./pages/AdminHomePage";
+import UserManagement from "./components/Admin/UserManagement";
 
 function App() {
 
@@ -36,6 +37,7 @@ function App() {
       <Route path="/admin" element={<AdminLayout />}>
         {/* Admin Layout */}
         <Route index element={<AdminHomePage />} />
+        <Route path="users" element={<UserManagement />} />
       </Route>
      </Routes>
     </BrowserRouter>
---
+++ frontend/src/components/Admin/UserManagement.jsx
@@ -0,0 +1,89 @@
+import React from 'react'
+
+const UserManagement = () => {
+
+    const users = [
+        { name: "John Doe", email: "john@example.com", role: "admin" },
+    ]
+
+    const [formData, setFormData] = React.useState({
+        name: "",
+        email: "",
+        password: "",
+        role: "customer" // Default role
+    });
+
+    const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("New user data:", formData);
+        // Reset form after submission
+        setFormData({
+            name: "",
+            email: "",
+            password: "",
+            role: "customer",
+        });
+    }
+
+  return (
+    <div className="max-w-7xl mx-auto p-6">
+      <h2 className="text-2xl font-bold mb-4">User Management</h2>
+      {/* Add new user form */}
+      <div className="p-6 rounded-lg mb-6">
+        <h3 className="text-lg font-bold mb-4">Add New User</h3>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-4">
+            <label className="block text-gray-700">Name</label>
+            <input
+              type="text"
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+              className="w-full p-2 border border-gray-200 rounded"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-700">Email</label>
+            <input
+              type="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              className="w-full p-2 border border-gray-200 rounded"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-700">Password</label>
+            <input
+              type="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+              className="w-full p-2 border border-gray-200 rounded"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-700">Role</label>
+            <select name="role" value={formData.role} onChange={handleChange} className="w-full p-2 border border-gray-200 rounded">
+                <option value="customer">Customer</option>
+                <option value="admin">Admin</option>
+            </select>
+          </div>
+          <button type='submit' className='bg-green-500 text-white py-2 px-4 rounded hover:bg-green-600'>Add User</button>
+        </form>
+      </div>
+
+      {/* User list */}
+      
+    </div>
+  );
+}
+
+export default UserManagement
