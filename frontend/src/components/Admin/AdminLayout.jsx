073ab5d7707ea12ab83ddaf40f5172db0ef3109b
aravindanirudh <aravindanirudh1@gmail.com>
2026-01-08 17:46:09 +0530

    AdminLayout fixes

--- frontend/src/components/Admin/AdminLayout.jsx
+++ frontend/src/components/Admin/AdminLayout.jsx
@@ -1,32 +1,38 @@
-import React from 'react'
-import { FaBars } from 'react-icons/fa'
-import AdminSidebar from './AdminSidebar';
-import { Outlet } from "react-router-dom"
+import React from "react";
+import { FaBars } from "react-icons/fa";
+import AdminSidebar from "./AdminSidebar";
+import { Outlet } from "react-router-dom";
 
 const AdminLayout = () => {
   const [isSidebarOpen, setIsSidebarOpen] = React.useState(false);
 
   const toggleSidebar = () => {
     setIsSidebarOpen(!isSidebarOpen);
-  }
+  };
 
   return (
-    <div className='min-h-screen flex flex-col md:flex-row relative'>
+    <div className="min-h-screen flex flex-col md:flex-row relative">
       {/* Mobile toggle button */}
       <div className="flex md:hidden p-4 bg-gray-900 text-white z-20">
         <button onClick={toggleSidebar}>
           <FaBars />
         </button>
-          <h1 className='ml-4 text-xl font-medium'>Admin Dashboard</h1>
+        <h1 className="ml-4 text-xl font-medium">Admin Dashboard</h1>
       </div>
       {/* Overlay for mobile sidebar */}
       {isSidebarOpen && (
-        <div className="fixed inset-0 z-10 bg-black/50 md:hidden" onClick={toggleSidebar}></div>
+        <div
+          className="fixed inset-0 z-10 bg-black/50 md:hidden"
+          onClick={toggleSidebar}
+        ></div>
       )}
 
       {/* Sidebar */}
-      <div className={`bg-gray-900 w-64 min-h-screen text-white absolute md:relative transform ${isSidebarOpen ? "translate-x-0" : "translate-x-full"} transition-transform duration-300 md:translate-x-0 md:static md:block z-20`}>
-        {/* Sidebar component */}
+      <div
+        className={`fixed inset-y-0 left-0 z-30 w-64 bg-gray-900 text-white transform transition-transform duration-300 ${
+          isSidebarOpen ? "translate-x-0" : "-translate-x-full"
+        } md:relative md:translate-x-0 md:z-auto`}
+      >
         <AdminSidebar />
       </div>
 
@@ -35,7 +41,7 @@ const AdminLayout = () => {
         <Outlet />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default AdminLayout
+export default AdminLayout;
