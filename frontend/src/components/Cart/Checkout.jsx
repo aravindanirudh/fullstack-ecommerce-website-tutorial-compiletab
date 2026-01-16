95f5fd95f22ae8be695550de036521bd9527c561
aravindanirudh <aravindanirudh1@gmail.com>
2026-01-16 19:15:17 +0530

    14:36:09 of tutorial completed

--- frontend/src/components/Cart/Checkout.jsx
+++ frontend/src/components/Cart/Checkout.jsx
@@ -1,29 +1,16 @@
-import React, { use } from "react";
+import React, { use, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import RazorpayButton from "./RazorpayButton";
-
-const cart = {
-  products: [
-    {
-      name: "Stylish Jacket",
-      size: "M",
-      color: "Black",
-      price: 120,
-      image: "https://picsum.photos/150?random=1",
-    },
-    {
-      name: "Casual Sneakers",
-      size: "42",
-      color: "White",
-      price: 75,
-      image: "https://picsum.photos/150?random=2",
-    },
-  ],
-  totalPrice: 195,
-};
+import { useDispatch, useSelector } from "react-redux";
+import { createCheckout } from "../../redux/slices/checkoutSlice";
+import axios from "axios";
 
 const Checkout = () => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { cart, loading, error } = useSelector((state) => state.cart);
+  const { user } = useSelector((state) => state.auth);
+
   const [checkoutId, setCheckoutId] = React.useState(null);
   const [shippingAddress, setShippingAddress] = React.useState({
     firstName: "",
@@ -35,15 +22,62 @@ const Checkout = () => {
     phone: "",
   });
 
-  const handleCreateCheckout = (e) => {
+  // Ensure cart is loaded before proceeding
+  useEffect(() => {
+    if (!cart || !cart.products || cart.products.length === 0) {
+      navigate("/homepage");
+    }
+  }, [cart, navigate]);
+
+  const handleCreateCheckout = async (e) => {
     e.preventDefault();
-    setCheckoutId(123);
+    if (cart && cart.products.length > 0) {
+      const res = await dispatch(createCheckout({
+        checkoutItems: cart.products,
+        shippingAddress,
+        paymentMethod: "Razorpay",
+        totalPrice: cart.totalPrice,
+      }));
+      if (res.payload && res.payload._id) {
+        setCheckoutId(res.payload._id); // Set checkout ID if checkout creation is successful
+      }
+    }
   };
 
-  const handlePaymentSuccess = (details) => {
-    console.log("Payment successful!", details);
-    navigate("/order-confirmation");
+  const handlePaymentSuccess = async (details) => {
+    try {
+      const response = await axios.put(`${import.meta.env.VITE_BACKEND_URL}/api/checkout/${checkoutId}/pay`, {
+        paymentStatus: "paid", paymentDetails: details
+      }, { headers: { Authorization: `Bearer ${localStorage.getItem("userToken")}` } }
+    );
+      await handleFinalizeCheckout(checkoutId); // Finalize checkout if payment is successful
+    } catch (error) {
+      console.error(error);
+    }
   };
+
+  const handleFinalizeCheckout = async (checkoutId) => {
+    try {
+      const response = await axios.post(`${import.meta.env.VITE_BACKEND_URL}/api/checkout/${checkoutId}/finalize`, {}, {
+        headers: { Authorization: `Bearer ${localStorage.getItem("userToken")}` }
+      });
+        navigate("/order-confirmation");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  if (loading) {
+    return <p>Loading cart...</p>;
+  }
+
+  if (error) {
+    return <p>Error: {error}</p>;
+  }
+
+  if (!cart || !cart.products || cart.products.length === 0) {
+    return <p>Your cart is empty!</p>;
+  }
 
   return (
     <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 max-w-7xl mx-auto py-10 px-6 tracking-tighter">
@@ -56,7 +90,7 @@ const Checkout = () => {
             <label className="block text-gray-700">Email</label>
             <input
               type="email"
-              value="user@example.com"
+              value={user ? user.email : ""}
               className="w-full p-2 border border-gray-200 rounded"
               disabled
             />
--- readme.md
+++ readme.md
@@ -63,6 +63,6 @@
 - Sonner library is used for toast notifications. To install it, do `npm install sonner` in terminal in the frontend folder
 - After 04:55:00 is the section to implement PayPal. Here, use PayPal Developer (Apps & Credentials > Create App > Merchant App) to create app for using in the MERN app. Also, download react-paypal-js package using npm as well. More info at https://www.npmjs.com/package/@paypal/react-paypal-js
 - But for some reason, in India, due to restrictions, PayPal sandbox is showing error for may users and so, it is unusable
-- For this reason, Razorpay might have to be used
+- For this reason, Razorpay might have to be used. Use UPI method and UPI ID success@razorpay for testing purposes
 - Using multer, cloudinary for image API (check package.json). Multer is used for handling file uploads. Cloudinary package helps us communicate with Cloudinary service. Streamifier is used to convert chunks of data into streams (for streaming data to APIs like Cloudinary)
 - Redux is used for simplification of state management and Redux Thunk is a middleware that helps us in handling asynchronous requests to the server
